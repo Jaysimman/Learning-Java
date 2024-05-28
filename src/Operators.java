293bde4be6cfc70fd8f6602808c60193f9c9ba38
Jaysimman <jaysimmannet@gmail.com>
2024-05-28 21:54:35 +0530

    Operators

--- src/Operators.java
+++ src/Operators.java
@@ -1,6 +1,11 @@
 public class Operators {
     public static void main(String[] args) {
         int a,b;
-        
+        a=10;b=2;
+        System.out.println(a+b);
+        System.out.println(a-b);
+        System.out.println(a/b);
+        System.out.println(a%b);
+        System.out.println(a*b);
     }
 }
