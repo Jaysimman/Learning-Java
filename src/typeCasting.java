a2dd9ca1631ccac8dbd91dfd7fd988b81fec325f
Jaysimman <jaysimmannet@gmail.com>
2024-05-28 21:37:46 +0530

    Type Casting

---
+++ src/typeCasting.java
@@ -0,0 +1,10 @@
+public class typeCasting {
+    public static void main(String[] args) {
+        int number = 10;
+        float number1 = number;
+        double myDouble = 9.77d;
+        int myint = (int) myDouble;
+        System.out.println(number1);
+        System.out.println(myint);
+    }
+}
