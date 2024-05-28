def88a417ee7994252e08e77afefaefa28281e0e
Jaysimman <jaysimmannet@gmail.com>
2024-05-28 21:47:07 +0530

    Adding Comments

--- src/typeCasting.java
+++ src/typeCasting.java
@@ -3,7 +3,7 @@ public class typeCasting {
         int number = 10;
         float number1 = number;
         double myDouble = 9.77d;
-        int myint = (int) myDouble;
+        int myint = (int) myDouble; // Manual type casting
         System.out.println(number1);
         System.out.println(myint);
     }
